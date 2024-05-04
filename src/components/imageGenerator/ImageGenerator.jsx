02f80831d463ef6ec8409d55bca185f39e0d2567
Mehmet <mehmetimdat.py@gmail.com>
2024-05-04 11:22:22 +0300

    edit

--- src/components/imageGenerator/ImageGenerator.jsx
+++ src/components/imageGenerator/ImageGenerator.jsx
@@ -10,14 +10,15 @@ const ImageGenerator = () => {
       return 0;
     }
 
+    console.log(inputRef.current.value);
+
     const response = await fetch(
       "https://api.openai.com/v1/images/generations",
       {
         method:"POST",
         headers:{
           "Content-Type":"application/json",
-          Authorization:"Bearer sk-proj-ew0Rf1DN2d286eeQRvo3T3BlbkFJIfisx0ZLCx3FpJzpGDHq",
-          "User-Agent":"Chrome",
+          Authorization:"Bearer sk-proj-0ntEUCPNRsz3kOyqtV7gT3BlbkFJdVPw9tVexZCnS8Tnp5qN",
         },
         body:JSON.stringify({
           prompt: `${inputRef.current.value}`,
@@ -40,13 +41,27 @@ const ImageGenerator = () => {
 
         <div className='flex flex-col mb-4'>
           <div className=''>
-            <img className='w-[512px] rounded-lg' src={imageUrl === "/" ? default_image : imageUrl} alt="default" />
+            <img 
+              className='w-[512px] rounded-lg' 
+              src={imageUrl === "/" ? default_image : imageUrl} 
+              alt="default" />
           </div>
         </div>
 
-        <div className='flex w-[800px] h-9 justify-between items-center rounded-full bg-slate-300'>
-          <input ref={inputRef} type="text" className='z-40 w-[600px] h-9 bg-transparent border-none outline-none text-sm text-black pl-[35px] mr-[35px] placeholder:text-zinc-900 ' placeholder='Specify the image you want.' />
-          <div onClick={() => {imageGenerator()}} className='z-50 flex items-center justify-center w-[250px] h-8 mr-[3px] text-sm rounded-full text-white bg-pink-700 cursor-pointer hover:bg-pink-600 transition-all ease-in-out delay-[40]'>
+        <div 
+          className='flex w-[800px] h-9 justify-between items-center rounded-full bg-slate-300'
+        >
+          
+          <input 
+            ref={inputRef} 
+            type="text" 
+            className='z-40 w-[600px] h-9 bg-transparent border-none outline-none text-sm text-black pl-[35px] mr-[35px] placeholder:text-zinc-900 ' placeholder='Specify the image you want.' 
+          />
+
+          <div 
+            onClick={() => {imageGenerator()}} 
+            className='z-50 flex items-center justify-center w-[250px] h-8 mr-[3px] text-sm rounded-full text-white bg-pink-700 cursor-pointer hover:bg-pink-600 transition-all ease-in-out delay-[40]'
+          >
             Generate
           </div>
         </div>
